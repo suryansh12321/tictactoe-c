07630c19b1fea8a75aeefcb5aac5bb78f3b9ef7f
suryansh12321 <suryanshpanda2005@gmail.com>
2026-05-14 17:16:39 +0530

    Added 2D array and printBoard function

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char board[3][3] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'}
+};
+
+void printBoard() {
+    
+    printf("\033[H\033[J"); 
+    
+    printf("\n=== Tic-Tac-Toe ===\n\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[0][0], board[0][1], board[0][2]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[1][0], board[1][1], board[1][2]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[2][0], board[2][1], board[2][2]);
+    printf("     |     |     \n\n");
+}
+
+int main() {
+    
+    printBoard();
+    return 0;
+}
