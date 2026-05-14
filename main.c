fff65cec6c778e4c7be605c7c8f4325c872ed235
suryansh12321 <suryanshpanda2005@gmail.com>
2026-05-14 20:58:42 +0530

    Added game loop, input mapping, and validation

--- main.c
+++ main.c
@@ -23,7 +23,47 @@ void printBoard() {
 }
 
 int main() {
+    int player = 1; 
+    int choice;
+    int row, col;
+    char mark;      
+
     
-    printBoard();
+    while (1) {
+        printBoard();
+        
+        
+        player = (player % 2) ? 1 : 2;
+
+        printf("Player %d, enter a number: ", player);
+        scanf("%d", &choice);
+
+        
+        mark = (player == 1) ? 'X' : 'O';
+
+        
+        row = (choice - 1) / 3;
+        col = (choice - 1) % 3;
+
+        
+        if (choice >= 1 && choice <= 9 && board[row][col] != 'X' && board[row][col] != 'O') {
+            
+        
+            board[row][col] = mark;
+            
+        
+            player++; 
+        } 
+        else {
+            printf("Invalid move! That spot is taken or out of bounds.\n");
+            
+        
+        
+            printf("Press Enter to try again...");
+            getchar(); 
+            getchar(); 
+        }
+    }
+
     return 0;
 }
